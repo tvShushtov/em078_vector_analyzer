c3afe314c43611097df7174c21b9de0081d81f37
pshuklin <shuklin.philipp@gmail.com>
2018-04-13 22:33:28 +0300

    Добавлен HPS.qsys

---
+++ hardware/_HPS/HPS/HPS_bb.v
@@ -0,0 +1,100 @@
+
+module HPS (
+	button_pio_external_connection_export,
+	clk_clk,
+	dipsw_pio_external_connection_export,
+	hps_0_f2h_cold_reset_req_reset_n,
+	hps_0_f2h_debug_reset_req_reset_n,
+	hps_0_f2h_stm_hw_events_stm_hwevents,
+	hps_0_f2h_warm_reset_req_reset_n,
+	hps_0_h2f_reset_reset_n,
+	hps_0_hps_io_hps_io_sdio_inst_CMD,
+	hps_0_hps_io_hps_io_sdio_inst_D0,
+	hps_0_hps_io_hps_io_sdio_inst_D1,
+	hps_0_hps_io_hps_io_sdio_inst_CLK,
+	hps_0_hps_io_hps_io_sdio_inst_D2,
+	hps_0_hps_io_hps_io_sdio_inst_D3,
+	hps_0_hps_io_hps_io_spim1_inst_CLK,
+	hps_0_hps_io_hps_io_spim1_inst_MOSI,
+	hps_0_hps_io_hps_io_spim1_inst_MISO,
+	hps_0_hps_io_hps_io_spim1_inst_SS0,
+	hps_0_hps_io_hps_io_uart0_inst_RX,
+	hps_0_hps_io_hps_io_uart0_inst_TX,
+	hps_0_hps_io_hps_io_i2c0_inst_SDA,
+	hps_0_hps_io_hps_io_i2c0_inst_SCL,
+	hps_0_hps_io_hps_io_i2c1_inst_SDA,
+	hps_0_hps_io_hps_io_i2c1_inst_SCL,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO09,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO35,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO40,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO53,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO54,
+	hps_0_hps_io_hps_io_gpio_inst_GPIO61,
+	led_pio_external_connection_export,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	reset_reset_n);	
+
+	input	[1:0]	button_pio_external_connection_export;
+	input		clk_clk;
+	input	[3:0]	dipsw_pio_external_connection_export;
+	input		hps_0_f2h_cold_reset_req_reset_n;
+	input		hps_0_f2h_debug_reset_req_reset_n;
+	input	[27:0]	hps_0_f2h_stm_hw_events_stm_hwevents;
+	input		hps_0_f2h_warm_reset_req_reset_n;
+	output		hps_0_h2f_reset_reset_n;
+	inout		hps_0_hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_0_hps_io_hps_io_sdio_inst_D0;
+	inout		hps_0_hps_io_hps_io_sdio_inst_D1;
+	output		hps_0_hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_0_hps_io_hps_io_sdio_inst_D2;
+	inout		hps_0_hps_io_hps_io_sdio_inst_D3;
+	output		hps_0_hps_io_hps_io_spim1_inst_CLK;
+	output		hps_0_hps_io_hps_io_spim1_inst_MOSI;
+	input		hps_0_hps_io_hps_io_spim1_inst_MISO;
+	output		hps_0_hps_io_hps_io_spim1_inst_SS0;
+	input		hps_0_hps_io_hps_io_uart0_inst_RX;
+	output		hps_0_hps_io_hps_io_uart0_inst_TX;
+	inout		hps_0_hps_io_hps_io_i2c0_inst_SDA;
+	inout		hps_0_hps_io_hps_io_i2c0_inst_SCL;
+	inout		hps_0_hps_io_hps_io_i2c1_inst_SDA;
+	inout		hps_0_hps_io_hps_io_i2c1_inst_SCL;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO09;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO35;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO40;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO53;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO54;
+	inout		hps_0_hps_io_hps_io_gpio_inst_GPIO61;
+	output	[6:0]	led_pio_external_connection_export;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	input		reset_reset_n;
+endmodule
